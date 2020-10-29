9b8a3cd76855b4d7b88f26a580469adf706d8216
j2onn2 <70683011+j2onn2@users.noreply.github.com>
2020-10-29 15:35:48 +0900

    .

--- main.c
+++ main.c
@@ -1,24 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 5
+#define SIZE 4
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
-	
-	int i	;
-	int a[SIZE]={1,2,3,5,5}	;
-	int b[SIZE]={1,2,3,4,5}	;
-	int flag_same = 1	;
-	
+void square_array(int a[], int size);
+void print_array(int a[],int size);
 
-	for(i=0; i<SIZE; i++)
-	{
-		if(a[i]!=b[i])
-		{
-			printf("배열은 다른 값을 가집니다.")	;
-			flag_same = 0	;
-		}
-	}
+int main(void) {
+	
+	int list[SIZE]={1,2,3,4}	;
+	
+	print_array(list, SIZE)	;
+	square_array(list, SIZE)	;
+	print_array(list, SIZE)	;
+	
 	return 0;
+}
+
+void square_array(int a[],int size)
+{
+	int i	;
+	
+	for(i=0;i<size; i++)
+	a[i]= a[i]*a[i]	;
+}
+
+void print_array(int a[], int size)
+{
+	int i	;
+	
+	for(i=0;i<size;i++)
+	printf("%3d", a[i]);
+	printf("\n");
 }
