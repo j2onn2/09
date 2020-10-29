9144007a5b0421eee78f567376abba9b01fe5644
j2onn2 <70683011+j2onn2@users.noreply.github.com>
2020-10-29 15:19:00 +0900

    .

--- main.c
+++ main.c
@@ -6,14 +6,21 @@
 
 int main(int argc, char *argv[]) {
 	
-	int i	;
+	int i, average	;
+	int sum	;
 	int grade[SIZE]	;
-	printf("5명의 점수를 입력하세요.\n")	;
 	
-	for( i=0 ; i<SIZE; i++)
+	sum=0	;
+	for(i=0; i<SIZE; i++)
+	{
+		
+	printf("학생 성적을 입력하세요: \n")	;
 	scanf("%d", &grade[i])	;
+	sum += grade [i];
+}
 	
-	for (i=0; i<SIZE; i++)
-	printf("grade[%d]= %d\n", i, grade[i])	;
+	average = sum/SIZE	;
+	printf("성적평균 : %d\n", average)	;
+	
 	return 0;
 }
